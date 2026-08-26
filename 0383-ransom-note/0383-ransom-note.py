42e2df963ddfe0b7cf7e06e81b95f30a8d543f65
Kartike Kumar <kartikelern@gmail.com>
2026-08-26 21:53:08 +0530

    Time: 15 ms (78.42%), Space: 19.7 MB (33.8%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,6 @@
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+            ransomNote_freq=Counter(ransomNote)
+            magazine_freq=Counter(magazine)
+            return all(ransomNote_freq[c] <= magazine_freq[c] for c in ransomNote)
